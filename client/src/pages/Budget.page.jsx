69c2caf38f682728a30253cae6d982c87d651fef
Agnieszka Przybysz <agnieszka.przybysz31@gmail.com>
2023-05-28 22:48:51 +0200

    Added changes
    Added new button. Fetched data from server with the use of react-query. In Budget.page.jsx data are displayed based on isLoading, isError and isSuccess states.

--- client/src/pages/Budget.page.jsx
+++ client/src/pages/Budget.page.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
-
-import { ActionHeader, Card, Page } from 'ui';
+import {
+  ActionHeader,
+  Card,
+  Page,
+  Button,
+  NoContent,
+  Loader,
+  Error,
+  Table,
+} from 'ui';
 import { Grid } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { BudgetService } from '../api';
 
 export const BudgetPage = () => {
+  const getBudgetData = async () => {
+    return await BudgetService.findAll();
+  };
+
+  const { isLoading, isError, isSuccess, data } = useQuery({
+    queryKey: ['budgetData'],
+    queryFn: () => getBudgetData(),
+  });
+
   return (
     <Page title="Budżet">
       <Card
@@ -11,12 +32,27 @@ export const BudgetPage = () => {
           <ActionHeader
             variant={'h1'}
             title="Budżet"
-            renderActions={() => null}
+            renderActions={() => (
+              <Button
+                variant={'contained'}
+                color={'primary'}
+                size={'large'}
+                startIcon={<AddIcon />}
+                className="button-iconleft"
+              >
+                Zdefiniuj budżet
+              </Button>
+            )}
           />
         }
       >
         <Grid container>
-          <Grid item xs={12}></Grid>
+          <Grid item xs={12} container justifyContent="center">
+            {isLoading && <Loader />}
+            {isError && <Error />}
+            {isSuccess && data.length < 0 && <NoContent />}
+            {isSuccess && data.length > 0 && <div>Tabela</div>}
+          </Grid>
         </Grid>
       </Card>
     </Page>
