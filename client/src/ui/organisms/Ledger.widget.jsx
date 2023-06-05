8a45f40c0af0bf0e82db11e3bd1484b082c4c7e2
Agnieszka Przybysz <agnieszka.przybysz31@gmail.com>
2023-06-05 23:39:27 +0200

    Added Table, NoContent, Error, Loader in Ledger.widget.jsk
    In Ledger.widget.jsx were implemented functionality conserning displaying data based on request status and user activity

--- client/src/ui/organisms/Ledger.widget.jsx
+++ client/src/ui/organisms/Ledger.widget.jsx
@@ -1,17 +1,154 @@
 import React from 'react';
 
-import { ActionHeader, Card } from 'ui';
+import {
+  ActionHeader,
+  Card,
+  Button,
+  NoContent,
+  Error,
+  Loader,
+  Table,
+  CategoryCell,
+  Money,
+  LocalizedDate,
+} from 'ui';
+import { Grid } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { LedgerService } from 'api';
 
 export const LedgerWidget = () => {
+  const getLedgerData = async () => {
+    return await LedgerService.findAll();
+  };
+
+  const { isLoading, isError, isSuccess, data } = useQuery({
+    queryKey: ['ledgerData'],
+    queryFn: () => getLedgerData(),
+  });
+
+  const columns = [
+    {
+      id: 'Name',
+      label: 'Nazwa',
+      renderCell(row) {
+        return row.name;
+      },
+    },
+    {
+      id: 'CategoryName',
+      label: 'Kategoria',
+      renderCell(row) {
+        return (
+          <CategoryCell name={row.categoryName} color={row.categoryColor} />
+        );
+      },
+    },
+    {
+      id: 'createdDate',
+      label: 'Data',
+      renderCell(row) {
+        return <LocalizedDate date={row.createdAt} />;
+      },
+    },
+    {
+      id: 'amountInCents',
+      label: 'Obecna kwota',
+      renderCell(row) {
+        return row.mode === 'EXPENSE' ? (
+          <div style={{ color: 'red' }}>
+            -<Money inCents={row.amountInCents} />
+          </div>
+        ) : row.mode === 'INCOME' ? (
+          <div style={{ color: 'green' }}>
+            +<Money inCents={row.amountInCents} />
+          </div>
+        ) : (
+          <Money inCents={row.amountInCents} />
+        );
+      },
+    },
+  ];
+  let rows = [];
+  if (isSuccess && data.length > 0) {
+    rows = data.map((item) => {
+      const properties = {
+        id: item.id,
+        name: item.title,
+        categoryName: item.category.name,
+        categoryColor: item.category.color,
+        createdAt: item.createdAt,
+        mode: item.mode,
+        amountInCents: item.amountInCents,
+      };
+      return properties;
+    });
+  }
+
+  const getUniqueId = (arr) => arr.id;
+
+  const queryClient = useQueryClient();
+  const deleteRecordsMutation = useMutation({
+    mutationFn: (ids) => {
+      return LedgerService.remove(ids);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['ledgerData'] });
+    },
+  });
+
+  const deleteRecords = (selectedRecords) =>
+    deleteRecordsMutation.mutate({ ids: selectedRecords });
+
   return (
-    <Card
-      title={
-        <ActionHeader
-          variant={'h1'}
-          title="Portfel"
-          renderActions={() => <></>}
-        />
-      }
-    />
+    <>
+      <Card
+        title={
+          <ActionHeader
+            variant={'h1'}
+            title="Portfel"
+            renderActions={() => (
+              <div>
+                <Button
+                  variant={'outlined'}
+                  color={'primary'}
+                  size={'large'}
+                  startIcon={<AddIcon />}
+                  className="button-iconleft"
+                >
+                  Wpłać
+                </Button>
+                <Button
+                  variant={'outlined'}
+                  color={'primary'}
+                  size={'large'}
+                  startIcon={<RemoveIcon />}
+                  className="button-iconleft"
+                >
+                  Wypłać
+                </Button>
+              </div>
+            )}
+          />
+        }
+      >
+        <Grid container>
+          <Grid item xs={12} container justifyContent="center">
+            {isLoading && <Loader />}
+            {isError && <Error />}
+            {isSuccess && data.length === 0 && <NoContent />}
+            {isSuccess && data.length > 0 && (
+              <Table
+                rows={rows}
+                headCells={columns}
+                getUniqueId={getUniqueId}
+                deleteRecords={deleteRecords}
+              />
+            )}
+          </Grid>
+        </Grid>
+      </Card>
+    </>
   );
 };
