fb11df123ce4e4f029cdfe963d2e83ea3fd47676
Agnieszka Przybysz <agnieszka.przybysz31@gmail.com>
2023-06-19 23:20:18 +0200

    added changes to Balance Chart after running cypress tests

--- client/src/ui/organisms/BalanceSheetChart.jsx
+++ client/src/ui/organisms/BalanceSheetChart.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { SummaryService } from 'api';
-import { Money, NoResult } from 'ui';
+import { Money, NoResult, CategoryCell } from 'ui';
+import { Typography, Box } from '@mui/material';
 
-import {
-  Chart as ChartJS,
-  ArcElement,
-  Tooltip,
-  Legend,
-  SubTitle,
-} from 'chart.js';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
 
-ChartJS.register(ArcElement, Tooltip, Legend, SubTitle);
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 export const BalanceSheetChart = () => {
   const getBalanceChartQuery = async () => {
@@ -38,29 +33,19 @@ export const BalanceSheetChart = () => {
         }
       : {};
 
-  //console.log(data);
-  //console.log(balanceData);
-
   const options = {
-    plugins: {
-      subtitle: {
-        display: true,
-        text: 'Pozostała kwota',
-        position: 'top',
-        align: 'start',
-        font: {
-          size: 20,
-        },
+    responsive: true,
+    layout: {
+      padding: {
+        left: 40,
+        right: 40,
+        top: 0,
+        bottom: 0,
       },
+    },
+    plugins: {
       legend: {
-        display: true,
-        position: 'bottom',
-        align: 'start',
-        labels: {
-          font: {
-            size: 20,
-          },
-        },
+        display: false,
       },
       tooltip: {
         callbacks: {
@@ -74,17 +59,31 @@ export const BalanceSheetChart = () => {
 
   return (
     <>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Typography variant="h4">Saldo</Typography>
+        <Typography variant="h3">
+          {<Money inCents={typeof data !== 'undefined' ? data.balance : 0} />}
+        </Typography>
+      </Box>
+      <Typography variant="subtitle1" sx={{ mb: 1 }}>
+        Pozostała kwota
+      </Typography>
       {isLoading && <NoResult />}
       {isError && <NoResult />}
-      {isSuccess && data == null && <NoResult />}
-      {isSuccess && data !== null && (
-        <div>
-          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-            <h4>Saldo</h4>
-            <h3>{<Money inCents={data.balance} />}</h3>
-          </div>
+      {isSuccess && balanceData.labels.length === 0 && <NoResult />}
+      {isSuccess && balanceData.labels.length > 0 && (
+        <Box>
           <Doughnut data={balanceData} options={options} />
-        </div>
+          <Box>
+            {data.spending.map((option) => (
+              <CategoryCell
+                key={option.categoryId}
+                name={option.categoryName}
+                color={option.categoryColor}
+              />
+            ))}
+          </Box>
+        </Box>
       )}
     </>
   );
